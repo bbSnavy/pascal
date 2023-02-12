3dd57ac2b6c8807f140d18bb06efaa75ed9810c5
snavy <1337snavy@gmail.com>
2023-02-12 23:57:40 +0100

    code format

--- main.hs
+++ main.hs
@@ -1,12 +1,14 @@
 import Data.List (intercalate)
+
 level 1 = [1]
 level n = do
-    let v = wrap (level (n - 1)) n
-    [(v !! (i - 0)) + (v !! (i - 1)) | i <- [1..n]]
+  let v = wrap (level (n - 1)) n
+  [(v !! (i - 0)) + (v !! (i - 1)) | i <- [1 .. n]]
 
 wrap list size = [0] ++ list ++ [0]
 
-pascal n = map level [1..n]
+pascal n = map level [1 .. n]
 
-main = putStrLn
+main =
+  putStrLn
     (intercalate "\n" [show v | v <- pascal 15])
