9bdb43e30df1bb365d8e66f3f8b6c4d791c95dd2
snavy <1337snavy@gmail.com>
2023-02-12 23:56:45 +0100

    working program

---
+++ main.hs
@@ -0,0 +1,12 @@
+import Data.List (intercalate)
+level 1 = [1]
+level n = do
+    let v = wrap (level (n - 1)) n
+    [(v !! (i - 0)) + (v !! (i - 1)) | i <- [1..n]]
+
+wrap list size = [0] ++ list ++ [0]
+
+pascal n = map level [1..n]
+
+main = putStrLn
+    (intercalate "\n" [show v | v <- pascal 15])
