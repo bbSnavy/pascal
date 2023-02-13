8d02870d8c1bc70f4d44dda8342746ff004798d4
snavy <1337snavy@gmail.com>
2023-02-13 10:39:34 +0100

    use unlines for formatting

--- main.hs
+++ main.hs
@@ -1,5 +1,3 @@
-import Data.List (intercalate)
-
 level 1 = [1]
 level n = do
   let v = wrap (level (n - 1)) n
@@ -9,6 +7,4 @@ wrap list size = [0] ++ list ++ [0]
 
 pascal n = map level [1 .. n]
 
-main =
-  putStrLn
-    (intercalate "\n" [show v | v <- pascal 15])
+main = putStrLn (unlines [show v | v <- pascal 15])
